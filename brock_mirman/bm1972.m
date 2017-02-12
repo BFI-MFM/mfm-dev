664592afe9be840ee03f6b311f6386db486e3c3b
vzhorin <zhorin@hotmail.com>
2017-02-12 17:42:18 -0600

    initial version

--- brock_mirman/bm1972.m
+++ brock_mirman/bm1972.m
@@ -5,19 +5,19 @@ tic;
 alpha = 1./3; %capital share
 beta = 0.97; %discount rate
 
-tol_val =1000; tol_val_min = 1e-9; %accuracy
+tol_val =1000; tol_val_k= 1e-9; tol_cheb=2e-3;%accuracy
 domain_k = [0 3];
 kfun = chebfun(@(k) k, domain_k);
-kfun0 =  0.1.*kfun.^alpha;
+k_pol0 =  0.1.*kfun.^alpha;
 i = 1;
-while (tol_val > tol_val_min)
-    kfun= chebfun(@(k) kfun0((kfun0(k))),domain_k, 'eps', tol_val_min, 'vectorize','splitting','on');
-    kprime = chebfun(@(k) kfun0(k).^(1-alpha).*kfun(k),domain_k, 'eps', tol_val_min, 'vectorize','splitting','on');
+while (tol_val > tol_val_k)
+    k_pol= chebfun(@(k) k_pol0((k_pol0(k))),domain_k, 'eps', tol_val_min, 'vectorize','splitting','on');
+    kprime = chebfun(@(k) k_pol0(k).^(1-alpha).*k_pol(k),domain_k, 'eps', tol_cheb, 'vectorize','splitting','on');
     
     dfun = chebfun(@(k) (k.^alpha+1./(alpha.*beta).*kprime(k))...
-        .*alpha.*beta./(1+alpha*beta),domain_k, 'eps', tol_val_min, 'vectorize','splitting','on');
-    tol_val = abs(max(dfun)-max(kfun0))./abs(max(kfun0));
-    kfun0 = dfun;
+        .*alpha.*beta./(1+alpha*beta),domain_k, 'eps', tol_cheb, 'vectorize','splitting','on');
+    tol_val = abs(max(dfun)-max(k_pol0))./abs(max(k_pol0));
+    k_pol0 = dfun;
     disp(i); disp(tol_val);
     i=i+1;
 end
@@ -27,7 +27,7 @@ xlabel('$k$', 'interpreter', 'latex', 'fontsize',12, 'Color', 'blue');
 ylabel('$k^\prime(k)$', 'interpreter', 'latex', 'fontsize',12, 'Color', 'blue');
 title('Exact and Numerical Solutions', 'interpreter', 'latex', 'fontsize',12, 'Color', 'blue');
 hold on
-plot(kfun0, '--r', 'LineWidth', 3);
+plot(k_pol0, '--r', 'LineWidth', 3);
 hold off
 save('bm1972.mat')
 toc;
