910c4471b0aa244c4d2853119eaf876a810d7ac0
vzhorin <zhorin@hotmail.com>
2017-02-13 13:23:49 -0600

    initial version

---
+++ demo.m
@@ -0,0 +1,2 @@
+rochet
+[sh_el, sh_el2, pr_el, pr_el2] = chebel('rochet');
--- set_path_mfm.m
+++ set_path_mfm.m
@@ -1,3 +1,4 @@
 root_folder = fileparts( mfilename('fullpath') );
 addpath( fullfile(root_folder, 'chebfun') );
-
+addpath( fullfile(root_folder, 'brock_mirman') );
+addpath( fullfile(root_folder, 'rochet') );
