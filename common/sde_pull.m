d13572670a46cca2d32385a17753b92b567f9517
vzhorin <zhorin@hotmail.com>
2017-02-28 14:35:04 -0600

    initial version

--- common/sde_pull.m
+++ common/sde_pull.m
@@ -2,9 +2,9 @@ function [pullval] = sde_pull(model_name)
 %computes sde pull: mu/sigma -sigma'/2
 %input: model name
 %output: pull value
+load (model_name);
 LW = 'linewidth'; lw = 1.6;  FS = 'fontsize'; 
 gg = linspace(domain_x(1), domain_x(2),200);
-load (model_name);
 pullval = feval(mucheb,gg)./feval(sigmacheb,gg)-feval(diff(sigmacheb)./2,gg);
 
 
